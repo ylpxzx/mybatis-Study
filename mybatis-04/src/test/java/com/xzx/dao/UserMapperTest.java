c48ba6184577a0057ef0419d06e8586b91e134e7
xzx <794859685@qq.com>
2021-02-02 09:10:02 +0800

    【修改】mybatis-04模块导入Log4j,编写Log4j配置文件，实现日志输出

--- mybatis-04/src/test/java/com/xzx/dao/UserMapperTest.java
+++ mybatis-04/src/test/java/com/xzx/dao/UserMapperTest.java
@@ -3,9 +3,14 @@ package com.xzx.dao;
 import com.xzx.pojo.User;
 import com.xzx.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 public class UserMapperTest {
+
+//    在类中使用日志输出对象
+    static Logger logger = Logger.getLogger(UserMapperTest.class);
+
     @Test
     public void allUser() {
         // 获取所有用户数据
@@ -23,6 +28,13 @@ public class UserMapperTest {
         System.out.println(user);
         // 关闭SqlSession
         sqlSession.close();
+    }
+
+    @Test
+    public void testLog4j(){
+        logger.info("info:进入testLog4j");
+        logger.debug("debug:进入testLog4j");
+        logger.error("error:进入testLog4j");
     }
 
 }
